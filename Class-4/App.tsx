cd6a666ec30004591762004339fd1fc07db882d8
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2026-05-29 23:00:54 +0530

    DynamicTab

--- Class-4/App.tsx
+++ Class-4/App.tsx
@@ -1,9 +1,57 @@
+// import * as React from 'react';
+
+// import DynamicStackNavigator from './src/navigator/Stack/DynamicStackNavigator';
+// import StaticStackNavigator from './src/navigator/Stack/StaticStackNavigator';
+
+
+// export default function App(){
+//   return <StaticStackNavigator/>
+// }
+
 import * as React from 'react';
-
+import { Text, View } from 'react-native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import DynamicStackNavigator from './src/navigator/Stack/DynamicStackNavigator';
-import StaticStackNavigator from './src/navigator/Stack/StaticStackNavigator';
+import DynamicTabNavigator from './src/navigator/Tabs/DynamicTabNavigator';
 
+function HomeScreen() {
+  const navigation = useNavigation();
 
-export default function App(){
-  return <StaticStackNavigator/>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')}>
+        Go to Profile
+      </Button>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
+    </View>
+  );
+}
+
+const MyTabs = createBottomTabNavigator({
+  screens: {
+    Home: HomeScreen,
+    Profile: ProfileScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(MyTabs);
+
+export default function App() {
+  return <DynamicTabNavigator />;
 }
